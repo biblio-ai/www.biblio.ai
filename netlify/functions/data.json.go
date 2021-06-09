63e83f1fec2d45e4ab1bb9705d86d3dc221d5517
Justin Kelly <justin@kelly.org.au>
2021-06-09 12:50:36 +1000

    data

---
+++ netlify/functions/data.json.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+  	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"net/http"
+	"log"
+	"os"
+	"io/ioutil"
+)
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	// read all response body
+      	//Get the path parameter that was sent
+	//uuid := request.PathParameters["uuid"]
+        uuid := request.QueryStringParameters["uuid"]
+
+	data := Request( "https://hgrqtmovrbvpvxjqungh.supabase.co/rest/v1/item?select=id,url,item_adult(value,score),item_brand(value,score),item_category(value,score),item_celebrity(value,score),item_color(black_and_white,accent_color,dominant_color_background,dominant_color_foreground,dominant_colors),item_description(value,score),item_face(gender,age),item_landmark(value,score),item_object(value,score),item_racy(value,score),item_tag(value,score),item_text(line,value),item_text_entity(value,match_text,text_type,text_sub_type,text_score),item_text_key_phrase(value),item_text_language(value,code,score),item_text_sentiment(score)&and=(id.eq."+uuid+")" )
+	// print `data` as a string
+        blah := string(data)
+	return &events.APIGatewayProxyResponse{
+		StatusCode:        200,
+		Headers:           map[string]string{"Content-Type": "text/plain"},
+		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+                Body:              blah,
+		IsBase64Encoded:   false,
+	}, nil
+}
+
+func main() {
+	
+    lambda.Start(handler)
+	// make a sample HTTP GET request
+	
+
+}
+
+
+func Request(url string) []byte {
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    req.Header.Set("apikey",  os.Getenv("SUPABASE_API_KEY"))
+    resp, err := http.DefaultClient.Do(req)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return body
+}
+
